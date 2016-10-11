787467b1307c814052b9d077352a9e3b73943860
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-10-11 14:54:22 -0700

    debugging "bundle not found"

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Link } from 'react-router'
-import { Button } from 'react-toolbox/lib/button';
 
 import GoodbyeWorld from '../containers/Goodbye'
 import HelloWorld from '../containers/Hello'
@@ -12,7 +11,6 @@ export default class App extends Component {
       <div>
         <div><Link to={'/hello'}> What up? </Link></div>
         <div><Link to={'/goodbye'}> No thanks! </Link></div>
-        <Button label='Whatup' />
         {this.props.children}
       </div>
     )
